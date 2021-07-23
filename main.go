c1dff6daecdaf7c38a74eadc68c1da8ac36f5c32
zqk <zhangqiankun@qiniu.com>
2021-07-23 15:46:36 +0800

    add custom response headers middleware

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"strings"
 )
 
 var (
@@ -11,11 +12,14 @@ var (
 	base string
 	// server addr
 	addr string
+	// custom response headers
+	headers string
 )
 
 func init() {
 	flag.StringVar(&base, "base", "./data", "tell me where to find files")
 	flag.StringVar(&addr, "addr", "127.0.0.1:18888", "tell me which host and port to listen on")
+	flag.StringVar(&headers, "headers", "", "tell me your custom response headers, format like 'Field1:Value1//Field2:Value2//Field3:Value3'")
 }
 
 func main() {
@@ -26,7 +30,24 @@ func main() {
 	if addr == "" {
 		log.Fatalf("you have to give me a addr")
 	}
-	http.Handle("/", http.FileServer(http.Dir(base)))
+	hdrs := strings.Split(strings.TrimSpace(headers), "//")
+	http.Handle("/", respHeaderswrapper(http.FileServer(http.Dir(base)), hdrs))
 	log.Printf("serving %s on addr: http://%s\n", base, addr)
 	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func respHeaderswrapper(hdlr http.Handler, headers []string) http.Handler {
+	f := func(w http.ResponseWriter, r *http.Request) {
+		for i := 0; i < len(headers); i++ {
+			if h := headers[i]; h != "" {
+				s := strings.Split(h, ":")
+				if len(s) != 2 {
+					continue
+				}
+				w.Header().Add(s[0], s[1])
+			}
+		}
+		hdlr.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(f)
 }
